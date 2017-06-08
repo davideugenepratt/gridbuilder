a69aa3523dda5e376506228241e347e0a5e29d47
David Pratt <David Pratt>
2017-06-07 23:26:19 -0700

    Initial commit

---
+++ gridbuilder.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     GridBuilder
+ * Plugin URI:      https://github.com/davideugenepratt/gridbuilder
+ * Description:     A Bootstrap based layout building plugin.
+ * Author:          David Pratt
+ * Author URI:      http://www.davideugenepratt.com
+ * Text Domain:     gridbuilder
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Gridbuilder
+ */
